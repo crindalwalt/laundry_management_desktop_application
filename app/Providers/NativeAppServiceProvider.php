196a026be8d75d15c7da8cc44dd6e7639487f06c
Shahzad Farooq <crindalwalt@protonmail.com>
2024-01-10 17:12:15 +0500

    desktop configuration and database setup complete (SF-01)

---
+++ app/Providers/NativeAppServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Native\Laravel\Facades\Window;
+use Native\Laravel\Contracts\ProvidesPhpIni;
+
+class NativeAppServiceProvider implements ProvidesPhpIni
+{
+    /**
+     * Executed once the native application has been booted.
+     * Use this method to open windows, register global shortcuts, etc.
+     */
+    public function boot(): void
+    {
+        Window::open()
+        ->width(1000)
+        ->height(700)
+        ->showDevTools(false)
+        ->toggleFullscreen(true);
+    }
+
+    /**
+     * Return an array of php.ini directives to be set.
+     */
+    public function phpIni(): array
+    {
+        return [
+        ];
+    }
+}
